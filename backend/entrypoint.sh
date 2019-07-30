9da1a7c4272d602faf2dbc4d9e66266c185a8817
Shaung Cheng <shaungc@umich.edu>
2019-07-29 23:17:48 -0700

    add collectstatic so static can upload to s3

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -26,7 +26,7 @@ python manage.py migrate
 
 # since we are hosting static files on s3, we no longer need to do this every time
 # python manage.py collectstatic --clear --noinput
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # echo Starting gunicorn...
 # how many workers: https://stackoverflow.com/questions/15979428/what-is-the-appropriate-number-of-gunicorn-workers-for-each-amazon-instance-type
