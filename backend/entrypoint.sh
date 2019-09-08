339524e392e997728e78f1370473878ca910460e
Shaung <shaungc@umich.edu>
2019-09-08 01:31:39 -0700

    switch to runserver

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -44,5 +44,5 @@ env
 
 DJANGO_SETTINGS_MODULE=django_backend.settings
 
-# python manage.py runserver 0.0.0.0:8000
-gunicorn django_backend.wsgi:application --workers=${WORKERS_NUM} --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
+# gunicorn django_backend.wsgi:application --workers=${WORKERS_NUM} --bind 0.0.0.0:8000
